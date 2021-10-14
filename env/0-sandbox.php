6f269865565f07eeb8698200dfecbfc62cae8df7
Ian Dunn <ian@iandunn.name>
2021-10-14 13:51:36 -0700

    Sandbox: Only inject BrowserSync when `sync` task is running.
    This prevents endless console warnings when the script is injected but can't connect to the task.

--- env/0-sandbox.php
+++ env/0-sandbox.php
@@ -11,14 +11,20 @@ defined( 'WPINC' ) || die();
 require_once WPMU_PLUGIN_DIR . '/wporg-mu-plugins/mu-plugins/blocks/global-header-footer/blocks.php';
 
 /*
- * Inject BrowserSync's watcher script, to inject changed files or reload the page.
+ * Add BrowserSync's watcher script, to inject changed files or reload the page.
  */
 add_action( 'wp_print_footer_scripts', function() {
+	$bs_port    = 3008; // Must match the one in `bs-config.js`.
+	$bs_running = is_resource( @fsockopen( 'localhost', $bs_port ) );
+
+	if ( ! $bs_running ) {
+		return;
+	}
+
 	?>
 
 	<script id="__bs_script__">//<![CDATA[
-		// Port must match the one in `bs-config.js`.
-		document.write( `<script async src=\'http://${location.hostname}:3008/browser-sync/browser-sync-client.js?v=2.27.5\'><\/script>` );
+		document.write( `<script async src=\'http://${location.hostname}:<?php echo absint( $bs_port ); ?>/browser-sync/browser-sync-client.js?v=2.27.5\'><\/script>` );
 		//]]>
 	</script>
 
