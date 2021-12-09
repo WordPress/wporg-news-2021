07c4659108b8f857b1de3d7e18c7330296ebf12d
Kelly Dwan <ryelle@users.noreply.github.com>
2021-12-09 16:45:12 -0500

    Latest Release: Update style to match design

--- source/wp-content/themes/wporg-news-2021/functions.php
+++ source/wp-content/themes/wporg-news-2021/functions.php
@@ -20,6 +20,7 @@ add_filter( 'post_class', __NAMESPACE__ . '\specify_post_classes', 10, 3 );
 add_filter( 'theme_file_path', __NAMESPACE__ . '\conditional_template_part', 10, 2 );
 add_filter( 'render_block_data', __NAMESPACE__ . '\custom_query_block_attributes' );
 add_filter( 'template_redirect', __NAMESPACE__ . '\jetpack_likes_workaround' );
+add_filter( 'the_title', __NAMESPACE__ . '\update_the_title', 10, 2 );
 
 /**
  * Register theme support.
@@ -348,4 +349,21 @@ function jetpack_likes_workaround() {
 	if ( is_callable( [ $jetpack_likes, 'load_styles_register_scripts' ] ) ) {
 		$jetpack_likes->load_styles_register_scripts();
 	}
+}
+
+/**
+ * Remove "WordPress" from the release post title.
+ *
+ * @param string $title The post title.
+ * @param int    $id    The post ID.
+ * @return string Filtered post title.
+ */
+function update_the_title( $title, $id ) {
+	// Remove "WordPress" from the post title in the Latest Release section on the front page.
+	$category_slugs = wp_list_pluck( get_the_category( $id ), 'slug' );
+	if ( is_front_page() && in_array( 'releases', $category_slugs ) ) {
+		return str_replace( 'WordPress', '', $title );
+	}
+
+	return $title;
 }
