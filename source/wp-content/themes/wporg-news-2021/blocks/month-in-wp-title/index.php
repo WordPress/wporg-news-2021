805ab05b72f0594b973e020520f13362ddb41e11
Kelly Dwan <ryelle@users.noreply.github.com>
2022-02-10 16:41:49 -0500

    Add "Month in WordPress Title" block (#290)
    * Add "Month in WordPress Title" block
    
    * Add aria-label to the heading too
    
    * Show correct month name, move year to heading tag, use aria-label just for longer month name
    
    * Update first/last in year class logic to use the content year instead of published year
    
    * Update the styles for the Month in WordPress page
    
    * Rename the block folder to match block name
    
    * Use a block.json to set up block data
    
    * Simplify function name
    
    * Explain the 0th day trick
    
    * Use `esc_js`
    
    * Adjust size of stroke

---
+++ source/wp-content/themes/wporg-news-2021/blocks/month-in-wp-title/index.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace WordPressdotorg\Theme\News_2021\Blocks\Month_In_WordPress;
+
+add_action( 'init', __NAMESPACE__ . '\register_block' );
+add_action( 'enqueue_block_assets', __NAMESPACE__ . '\register_block_type_js' );
+
+/**
+ * Renders the `wporg/month-in-wp-title` block on the server.
+ *
+ * @param array    $attributes Block attributes.
+ * @param string   $content    Block default content.
+ * @param WP_Block $block      Block instance.
+ *
+ * @return string Returns the release version for the current post wrapped inside "h1" tags.
+ */
+function render_block( $attributes, $content, $block ) {
+	if ( ! isset( $block->context['postId'] ) ) {
+		return '';
+	}
+
+	$post_id  = $block->context['postId'];
+	$tag_name = 'h2';
+
+	if ( isset( $attributes['level'] ) ) {
+		$tag_name = 0 === $attributes['level'] ? 'p' : 'h' . $attributes['level'];
+	}
+
+	// Getting the "0th" of the month actually gets the last day of the previous month, so for a post published on
+	// June 5th 2021, this will return a date object for midnight on May 31st 2021. This ensures we get the correct
+	// previous year for the December recap.
+	$date = date_create_from_format( 'Y-m-d H:i:s', get_the_date( 'Y-m-00 00:00:00', $post_id ) );
+
+	$short_month = $date->format( 'M' );
+	$long_month = $date->format( 'F' );
+	$year = $date->format( 'Y' );
+
+	$wrapper_attributes = get_block_wrapper_attributes();
+
+	if ( isset( $attributes['isLink'] ) && $attributes['isLink'] ) {
+		return sprintf(
+			'<%1$s %2$s><a href="%3$s"><span aria-label="%4$s">%5$s</span> %6$s</a></%1$s>',
+			$tag_name,
+			$wrapper_attributes,
+			get_permalink( $post_id ),
+			$long_month,
+			$short_month,
+			$year
+		);
+	}
+
+	return sprintf(
+		'<%1$s %2$s><span aria-label="%3$s">%4$s</span> %5$s</%1$s>',
+		$tag_name,
+		$wrapper_attributes,
+		$long_month,
+		$short_month,
+		$year
+	);
+}
+
+/**
+ * Registers the `wporg/month-in-wp-title` block on the server.
+ */
+function register_block() {
+	register_block_type(
+		__DIR__,
+		array(
+			'render_callback' => __NAMESPACE__ . '\render_block',
+		)
+	);
+}
+
+/**
+ * Register block type in JS, for the editor.
+ */
+function register_block_type_js() {
+	$block = wp_json_file_decode( __DIR__ . '/block.json' );
+	ob_start();
+	?>
+	( function( wp ) {
+		wp.blocks.registerBlockType(
+			'<?php echo esc_js( $block->name ); ?>',
+			{
+				title: '<?php echo esc_js( $block->title ); ?>',
+				edit: function( props ) {
+					return wp.element.createElement( wp.serverSideRender, {
+						block: '<?php echo esc_js( $block->name ); ?>',
+						attributes: props.attributes
+					} );
+				},
+			}
+		);
+	}( window.wp ));
+	<?php
+	wp_add_inline_script( 'wp-editor', ob_get_clean(), 'after' );
+}
