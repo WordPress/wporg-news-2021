665edb38215ce616db952341dce94290e75184e2
keyurptl <shailm@itpathsolutions.com>
2024-02-16 10:09:38 +0530

    Added Global Documentation in functions.php

--- source/wp-content/themes/wporg-news-2021/functions.php
+++ source/wp-content/themes/wporg-news-2021/functions.php
@@ -154,6 +154,8 @@ function clarify_body_classes( $classes ) {
 
 /**
  * Add post classes to help make possible some design elements such as spacers between groups of posts.
+ * 
+ * @global WP_Query $wp_query WordPress Query object.
  *
  * @param array $classes
  *
